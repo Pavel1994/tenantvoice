8ed8e7c2e71e066b9b2cdd820b52c7e6b7b66f4e
Pavel1994 <pavel3407@gmail.com>
2026-04-07 18:40:29 +0300

    fix next config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config */
+};
+
+module.exports = nextConfig;
+
+/*import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+//};
+
+//export default nextConfig;
